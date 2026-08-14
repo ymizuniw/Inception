819cb4f5b89e18cff8a6405e91f1ee00483aea5f
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2026-08-14 19:07:33 +0900

    update: install_composer.sh is written.

--- srcs/requirements/wordpress/tools/install_composer.sh
+++ srcs/requirements/wordpress/tools/install_composer.sh
@@ -2,5 +2,5 @@
 set -e
 
 curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
